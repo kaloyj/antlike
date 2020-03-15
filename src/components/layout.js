0941e027808e0dc9925fcf49757d19d109d4528c
Carlo Janea <janeacarlo@gmail.com>
2020-03-15 18:42:46 +0800

    initial css boilerplate

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,77 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Global, css } from "@emotion/core"
+
+export const COLOR_SCHEME = {
+  accent: "#D7263D",
+  darkRed: "#990033",
+  fontColor: "#211103",
+  lightYellow: "#EEEFA8",
+}
 
 const Layout = ({ children }) => {
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+          }
+
+          html,
+          body {
+            margin: 0;
+            font-family: "DM Sans", -apple-system, BlinkMacSystemFont, Roboto,
+              Helvetica, sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: ${COLOR_SCHEME.fontColor};
+            line-height: 1.1;
+          }
+
+          .d-flex {
+            display: flex;
+            flex-flow: row wrap;
+
+            &.column {
+              flex-direction: column;
+            }
+
+            &.no-wrap {
+              flex-wrap: nowrap;
+            }
+          }
+
+          .f-100 {
+            flex: 0 0 100%;
+          }
+
+          .f-92 {
+            flex: 0 0 92%;
+          }
+        `}
+      ></Global>
+
+      <main
+        className="d-flex"
+        css={css`
+          width: 100vw;
+          min-height: 100vh;
+          border: 3px solid black;
+          overflow-x: hidden;
+        `}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+        {children}
+      </main>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,41 @@
 import React from "react"
-
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi ants</h1>
+    {/* landing */}
+    <div
+      className="d-flex f-92"
+      css={css`
+        margin-left: 4%;
+        justify-content: center;
+        margin-top: 1.5rem;
+        border: 3px solid blue;
+
+        @media only screen and (min-width: 768px) {
+        }
+
+        @media screen and (min-width: 1024px) {
+          margin-top: 2rem;
+          justify-content: flex-start;
+        }
+      `}
+    >
+      <h1
+        css={css`
+          font-size: 2.25rem;
+
+          @media screen and (min-width: 1024px) {
+            font-size: 3rem;
+          }
+        `}
+      >
+        antlike
+      </h1>
+    </div>
   </Layout>
 )
 
